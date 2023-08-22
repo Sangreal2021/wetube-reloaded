172b42d29dff31f31e9f3adf0cc630c9089e28b6
Ham ilwoo <hiw051515@gmail.com>
2023-08-22 09:24:02 +0900

    20230822_a

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -7,7 +7,7 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
+// globalRouter.get("/search", search);
 
 // 다른 파일에서 이 라우터를 import하려면 아래처럼 export해야함.
 export default globalRouter;
