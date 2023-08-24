5a639930badb8c49f1cfcd640764233ed1295ba3
Ham ilwoo <hiw051515@gmail.com>
2023-08-24 10:04:57 +0900

    wetube_20230824

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { watch, getEdit, postEdit, upload, deleteVideo } from "../controllers/videoController";
+import {
+    watch,
+    getEdit,
+    postEdit,
+    getUpload,
+    postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
-// videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-// videoRouter.get("/upload", upload);
+// getEdit 과 postEdit을 합친 것
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
